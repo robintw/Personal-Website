1a9cbf731592a966cc96f40e236792cc1ddc12dd
Robin Wilson <r.t.wilson@rmplc.co.uk>
2010-06-27 06:45:30 -0700

    First commit

---
+++ contact.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<?php include 'stylesheets.php' ?>
+	<title>Robin Wilson - Contact</title>
+</head>
+<body>
+	<div class="container">
+	<h1 class="header">Robin Wilson</h1>
+	<hr>
+	<h2 class="alt">Contact</h2>
+	<hr>
+	<?php include 'navigation.php' ?>
+	<div class="span-20 last">
+		<p>I can be contacted using the details below. I will endeavour to reply to you as soon as possible.</p>
+		<h6>Email:</h6>
+		<p><a href="mailto:robin@rtwilson.com">robin@rtwilson.com</a></p>
+		
+		<h6>Other:</h6>
+		<p>If you need my address or telephone number then please contact me.</p>
+	</div>
+</div>
+<?php include 'footer.php' ?>
+<!-- Piwik -->
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.rtwilson.com/" : "http://piwik.rtwilson.com/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 7);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://piwik.rtwilson.com/piwik.php?idsite=7" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tag -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<?php include 'stylesheets.php' ?>
+	<title>Robin Wilson - Personal Homepage</title>
+</head>
+<body>
+	<div class="container">
+	<h1 class="header">Robin Wilson</h1>
+	<hr>
+	<h2 class="alt">Programming, Craft, Photography and even a bit of thinking...</h2>
+	<hr>
+	<?php include 'navigation.php' ?>
+	<div class="span-20 last">
+		<p><img src="me.jpg" class="top right" alt="Me at Kenilworth Castle">
+		Hi, I'm Robin and this is my personal website.
+		</p>
+		<div class="span-12"><p class="warning error nogap">This website is under construction, therefore some links may not work.</p></div>
+	</div>
+</div>
+<?php include 'footer.php' ?>
+<!-- Piwik -->
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.rtwilson.com/" : "http://piwik.rtwilson.com/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 7);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://piwik.rtwilson.com/piwik.php?idsite=7" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tag -->
+</body>
+</html>
